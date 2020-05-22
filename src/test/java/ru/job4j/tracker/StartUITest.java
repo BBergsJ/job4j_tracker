b6fb94a67d89bd24cc750736664b322462028c40
BBergsJ <goldlike888@gmail.com>
2020-05-23 00:22:31 +0300

    Тесты вывода на консоль в StartUI[33585#272644]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,45 +1,15 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-//    @Test
-//    public void whenAddItem() {
-//        UserAction[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//
-//        new StartUI().init(input, tracker, answers);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
-//
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {item.getId(), "replaced item"};
-//        StartUI.editItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
-//    }
-//
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);;
-//        String[] arrayDelItem = {item.getId()};
-//        StartUI.deleteItem(new StubInput(arrayDelItem), tracker);
-//        Item deleted = tracker.findById(item.getId());
-//        Item expected = null;
-//        assertThat(deleted, is(expected));
-//    }
 
     @Test
     public void whenExit() {
@@ -48,4 +18,24 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserAction[] { action });
         assertThat(action.isCall(), is(true));
     }
+
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+
 }

e0cd3c6e29dcafa7388e264cce840a0d3c69e762
BBergsJ <goldlike888@gmail.com>
2020-05-31 00:36:13 +0300

    Рефакторинг - Шаблон Декоратор для валидатора

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -13,8 +13,7 @@ public class ValidateInputTest {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-        String[] data = {"one", "1"};
-        ValidateStubInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(new StubInput(new String[] {"one", "1"}));
         input.askInt("Enter");
         assertThat(mem.toString(), is(String.format("Please enter validate data again.%n")));
         System.setOut(out);
@@ -25,9 +24,7 @@ public class ValidateInputTest {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-
-        String[] data = {"7", "1"};
-        ValidateStubInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(new StubInput(new String[] {"7", "1"}));
         input.askInt("Enter", 6);
         assertThat(mem.toString(), is(String.format("Please select key from menu.%n")));
         System.setOut(out);
